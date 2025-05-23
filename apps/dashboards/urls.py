52e4c85b076366563d1a929eb8d34420da4b05ce
breina <breina.adenig123@gmail.com>
2025-05-23 22:14:56 +0800

    added initail models

--- apps/dashboards/urls.py
+++ apps/dashboards/urls.py
@@ -1,12 +1,28 @@
 from django.urls import path
 from .views import DashboardsView
 
+from .views import (
+    PieCountbySeverity,
+    LineCountbyMonth,
+    MultilineIncidentTop3Country,
+    multipleBarbySeverity,
+    FireStationView,)
 
+# urlpatterns = [
+#     path(
+#         "",
+#         DashboardsView.as_view(template_name="dashboard_analytics.html"),
+#         name="index",
+#     )
+# ]
 
 urlpatterns = [
-    path(
-        "",
-        DashboardsView.as_view(template_name="dashboard_analytics.html"),
-        name="index",
-    )
-]
+    # charts
+    path('', DashboardsView.as_view(template_name="dashboard_analytics.html"), name="index"),
+    path('dashboard/analytics/', DashboardsView.as_view(template_name="dashboard_analytics.html"), name='dashboard-chart'),
+    path("chart/", PieCountbySeverity, name="chart"),
+    path("lineChart/", LineCountbyMonth, name="chart"),
+    path("multilineChart/", MultilineIncidentTop3Country, name="chart"),
+    path("multiBarChart/", multipleBarbySeverity, name="chart"),
+    # path("/", FireStationView.as_view(), name="fire_stations_count"),
+    ]
